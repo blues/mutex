bf18b3e0f704420dd7d5117aab51526082ec47c7
Ray Ozzie <ray@ozzie.net>
2022-04-17 09:12:04 -0400

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/blues/mutex
+
+go 1.16
---
+++ mutex.go
@@ -0,0 +1,43 @@
+// Copyright 2022 Blues Inc.  All rights reserved.
+// Use of this source code is governed by licenses granted by the
+// copyright holder including that found in the LICENSE file.
+
+package mutex
+
+import (
+	"sync"
+)
+
+type Mutex struct {
+	Class string
+	m     sync.Mutex
+}
+
+func (m *Mutex) Lock() {
+	m.m.Lock()
+}
+
+func (m *Mutex) Unlock() {
+	m.m.Unlock()
+}
+
+type RWMutex struct {
+	Class string
+	m     sync.RWMutex
+}
+
+func (m *RWMutex) Lock() {
+	m.m.Lock()
+}
+
+func (m *RWMutex) Unlock() {
+	m.m.Unlock()
+}
+
+func (m *RWMutex) RLock() {
+	m.m.RLock()
+}
+
+func (m *RWMutex) RUnlock() {
+	m.m.RUnlock()
+}
